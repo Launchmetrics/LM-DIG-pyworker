5228e81ca3f81cfeee127c310effece2e984e060
David Pecchioli <da.pecchioli@gmail.com>
2025-05-19 18:58:54 +0200

    /v1/chat/completions added

--- workers/tgi/server.py
+++ workers/tgi/server.py
@@ -15,7 +15,9 @@ MODEL_SERVER_URL = "http://0.0.0.0:5001"
 
 # This is the last log line that gets emitted once comfyui+extensions have been fully loaded
 MODEL_SERVER_START_LOG_MSG = '"message":"Connected","target":"text_generation_router::server"'
-MODEL_SERVER_ERROR_LOG_MSGS = ["Error: WebserverFailed", "Error: DownloadError", "Error: ShardCannotStart"]
+MODEL_SERVER_ERROR_LOG_MSGS = [
+    "Error: WebserverFailed", "Error: DownloadError", "Error: ShardCannotStart"
+]
 
 
 logging.basicConfig(
@@ -24,6 +26,34 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 log = logging.getLogger(__file__)
+
+
+@dataclasses.dataclass
+class ChatHandler(EndpointHandler[InputData]):
+
+    @property
+    def endpoint(self) -> str:
+        return "/v1/chat/completions"
+
+    @classmethod
+    def payload_cls(cls) -> Type[InputData]:
+        return InputData
+
+    def make_benchmark_payload(self) -> InputData:
+        return InputData.for_test()
+
+    async def generate_client_response(
+        self, client_request: web.Request, model_response: ClientResponse
+    ) -> Union[web.Response, web.StreamResponse]:
+        _ = client_request
+        match model_response.status:
+            case 200:
+                log.debug("SUCCESS")
+                data = await model_response.json()
+                return web.json_response(data=data)
+            case code:
+                log.debug("SENDING RESPONSE: ERROR: unknown code")
+                return web.Response(status=code)
 
 
 @dataclasses.dataclass
@@ -106,6 +136,7 @@ async def handle_ping(_):
 
 
 routes = [
+    web.post("/v1/chat/completions", backend.create_handler(ChatHandler())),
     web.post("/generate", backend.create_handler(GenerateHandler())),
     web.post("/generate_stream", backend.create_handler(GenerateStreamHandler())),
     web.get("/ping", handle_ping),
