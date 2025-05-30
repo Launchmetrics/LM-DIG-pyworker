970583735c43ea004db2516657bb6bd8dfb7ce57
David Pecchioli <da.pecchioli@gmail.com>
2025-05-30 18:40:04 +0200

    added do_sample in InputData()

--- workers/tgi/data_types.py
+++ workers/tgi/data_types.py
@@ -56,6 +56,18 @@ class InputData(ApiPayload):
     max_tokens: int
     temperature: Optional[float] = None
     num_beams: Optional[int] = None
+    do_sample: Optional[bool] = None
+
+    # logprobs: Optional[bool] = None
+    # frequency_penalty: Optional[float] = None
+    # model: Optional[str] = None
+    # presence_penalty: Optional[float] = None
+    # response_format: Optional[str] = None
+    # seed: Optional[int] = None
+    # stream: Optional[bool] = None
+    # stream_options: Optional[Any] = None
+    # top_logprobs: Optional[int] = None
+    # top_p: Optional[float] = None
 
     @classmethod
     def for_test(cls) -> "InputData":
