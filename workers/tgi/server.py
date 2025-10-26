77a9b86c84ea2af037272c62942918b8566f80aa
David Pecchioli <da.pecchioli@gmail.com>
2025-10-26 20:44:22 +0100

    added tgi_metrics in handle_ping()

--- workers/tgi/server.py
+++ workers/tgi/server.py
@@ -110,14 +110,25 @@ async def handle_ping(_):
     Return same metrics sent to autoscaler server
     According to lib.metrics.__send_metrics_and_reset compute_autoscaler_data
     """
-    tgi_metrics = {'q_size': 24}
+    session = backend.session
+    tgi_url = f'{MODEL_SERVER_URL}/metrics'  # TGI metrics
+    metrics = ['tgi_queue_size']  # params to parse
+    tgi_metrics = {}
+
+    async with session.get(tgi_url, timeout=5) as resp:
+        if resp.status == 200:
+            raw_resp = await resp.text()
+            for line in raw_resp.split('\n'):
+                for metric in metrics:
+                    if line.startswith(metric):
+                        tgi_metrics[metric] = int(line[len(metric):].strip())
+
     return web.json_response(
         {
             **backend.metrics.last_metrics,
             **tgi_metrics
         }
     )
-    #return web.json_response(backend.metrics.last_metrics)
 
 routes = [
     web.post("/v1/chat/completions", backend.create_handler(ChatHandler())),
