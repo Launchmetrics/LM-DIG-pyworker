b96db285465742c3f349339423ad06bae0cf513e
David Pecchioli <da.pecchioli@gmail.com>
2025-12-01 12:26:42 +0100

    make handle_clear async again

--- workers/tgi/server.py
+++ workers/tgi/server.py
@@ -146,11 +146,11 @@ async def handle_model_log_history(_):
     )
 
 
-def handle_clear(backend):
+async def handle_clear(backend):
     """
     Clear pyworker status to handle 24/7 expected behaviour
     """
-    backend.clear()
+    await backend.clear()
     return web.json_response({'status': 'ok', 'pyworker': 'cleared'})
 
 
