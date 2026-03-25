2e268c6b3c8e8e9ded909028bd3cc26d041857e6
David Pecchioli <david.pecchioli@launchmetrics.com>
2026-03-25 12:12:50 +0100

    updated MODEL_SERVER START and ERROR msgs

--- workers/openai/server.py
+++ workers/openai/server.py
@@ -15,17 +15,11 @@ MODEL_SERVER_URL = "http://0.0.0.0:5001"
 
 # This is the last log line that gets emitted once comfyui+extensions have been fully loaded
 MODEL_SERVER_START_LOG_MSG = [
-    '"message":"Connected","target":"text_generation_router"',
-    '"message":"Connected","target":"text_generation_router::server"',
+    "Application startup complete.",  # vLLM
 ]
 MODEL_SERVER_ERROR_LOG_MSGS = [
-    'Error: ShardFailed',
-    '"message":"shard terminated"',
-    '"message":"Terminating webserver"',
-    '"message":"Shutting down shards"',
-    'Error: WebserverFailed',
-    'Error: DownloadError',
-    'Error: ShardCannotStart',
+    "INFO exited: vllm",  # vLLM
+    "RuntimeError: Engine",  # vLLM
 ]
 
 
