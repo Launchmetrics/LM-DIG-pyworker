c21c186930fd4059754fd50af63b34b2554a2c83
David Pecchioli <david.pecchioli@launchmetrics.com>
2026-03-25 13:44:44 +0100

    added getenv for MODEL_SERVER_URL

--- workers/openai/server.py
+++ workers/openai/server.py
@@ -11,7 +11,7 @@ from lib.server import start_server
 from .data_types import InputData
 
 
-MODEL_SERVER_URL = "http://0.0.0.0:5001"
+MODEL_SERVER_URL = os.getenv('MODEL_SERVER_URL', 'http://127.0.0.1:8000')
 
 # This is the last log line that gets emitted once comfyui+extensions have been fully loaded
 MODEL_SERVER_START_LOG_MSG = [
