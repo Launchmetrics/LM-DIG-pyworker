10f0933833a1792b7151814cde222882b9df8fcc
David Pecchioli <david.pecchioli@launchmetrics.com>
2026-03-25 19:14:56 +0100

    added vLLM openai compatible params

--- workers/openai/data_types.py
+++ workers/openai/data_types.py
@@ -1,5 +1,4 @@
 import dataclasses
-import inspect
 from typing import Dict, Any, Optional
 from lib.data_types import ApiPayload, JsonDataException
 from tasks.brand import bench_messages
@@ -17,76 +16,50 @@ def no_default_str(cls):  # Decorator for class.
     return cls
 
 
+# https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams
+
 @dataclasses.dataclass
 @no_default_str
 class InputData(ApiPayload):
-    messages: list
-    max_tokens: int
-    stream: bool = False
+    messages: list  # mandadatory
+
     req_id: Optional[str] = None
-    repetition_penalty: Optional[float] = None
-    frequency_penalty: Optional[float] = None
-    logit_bias: Optional[list[float]] = None
-    logprobs: Optional[bool] = None
-    top_logprobs: Optional[int] = None
-    n: Optional[int] = None
-    presence_penalty: Optional[float] = None
-    seed: Optional[int] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.max_tokens
+    max_tokens: Optional[int] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.temperature
     temperature: Optional[float] = None
-    top_p: Optional[float] = None
-    tools: Optional[list] = None
-    tool_prompt: Optional[str] = None
-    tool_choice: Optional[str] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.logprobs
+    logprobs: Optional[int] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.n
+    n: Optional[int] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.presence_penalty
+    presence_penalty: Optional[float] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.repetition_penalty
+    repetition_penalty: Optional[float] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.seed
+    seed: Optional[int] = None
+
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.stop
     stop: Optional[list[str]] = None
 
-    """
-        Given a list of messages, generate a response.
-        - https://github.com/huggingface/text-generation-inference/blob/main/clients/python/text_generation/client.py
-        Check ChatRequest:
-        - https://github.com/huggingface/text-generation-inference/blob/main/clients/python/text_generation/types.py
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.top_k
+    top_k: Optional[int] = None
 
-        Args:
-            messages (`List`):
-                List of messages (dict) in the conversation
-            repetition_penalty (`float`):
-                The parameter for repetition penalty. 0.0 means no penalty. See [this
-                paper](https://arxiv.org/pdf/1909.05858.pdf) for more details.
-            frequency_penalty (`float`):
-                The parameter for frequency penalty. 0.0 means no penalty
-                Penalize new tokens based on their existing frequency in the text so far,
-                decreasing the model's likelihood to repeat the same line verbatim.
-            logit_bias (`List[float]`):
-                Adjust the likelihood of specified tokens
-            logprobs (`bool`):
-                Include log probabilities in the response
-            top_logprobs (`int`):
-                Include the `n` most likely tokens at each step
-            max_tokens (`int`):
-                Maximum number of generated tokens
-            n (`int`):
-                Generate `n` completions
-            presence_penalty (`float`):
-                The parameter for presence penalty. 0.0 means no penalty. See [this
-                paper](https://arxiv.org/pdf/1909.05858.pdf) for more details.
-            stream (`bool`):
-                Stream the response
-            seed (`int`):
-                Random sampling seed
-            temperature (`float`):
-                The value used to module the logits distribution.
-            top_p (`float`):
-                If set to < 1, only the smallest set of most probable tokens with probabilities that add up to `top_p` or
-                higher are kept for generation
-            tools (`List[Tool]`):
-                List of tools to use
-            tool_prompt (`str`):
-                A prompt to be appended before the tools
-            tool_choice (`str`):
-                The tool to use
-            stop (`List[str]`):
-                Stop generating tokens if a member of `stop` is generated
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.top_p
+    top_p: Optional[float] = None
 
-    """
+    # https://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.logit_bias
+    logit_bias: Optional[dict[int, float]] = None
+
+    # fhttps://docs.vllm.ai/en/stable/api/vllm/#vllm.SamplingParams.frequency_penalty
+    frequency_penalty: Optional[float] = None
 
     @classmethod
     def for_test(cls) -> "InputData":
@@ -102,7 +75,7 @@ class InputData(ApiPayload):
         return self.max_tokens
 
     @classmethod
-    def from_json_msg(cls, batch: list[Dict[str, Any]]) -> "InputData":
+    def from_json_msg(cls, batch: list[Dict[str, Any]]) -> list["InputData"]:
         errors = {}
         for json_msg in batch:
             for field in dataclasses.fields(cls):
